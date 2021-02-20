68a2087a1be0d483dd2eea31256769e01abbd19e
sahilss <sahilsharma.ss387@gmail.com>
2021-02-20 23:32:32 +0530

    setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
 const path = require('path');
 const hbs = require('hbs');
 const geocode = require('./utils/geocode');
@@ -93,6 +94,6 @@ app.get('*', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log("server is started");
 });
